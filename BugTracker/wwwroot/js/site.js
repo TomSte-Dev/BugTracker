dc128ad3e1f9917878b00ff7deb9f2cb6ab61650
Tom Stevens <TomStevensDev@gmail.com>
2024-05-01 15:22:48 +0100

    Feat: Initial layout change for dashboard

--- BugTracker/wwwroot/js/site.js
+++ BugTracker/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#sidebar-toggler').click(function () {
+        $('#left-sidebar').toggle();
+    });
+
+    $(window).resize(function () {
+        if ($(window).width() > 768) { // Adjust the width threshold as needed
+            $('#left-sidebar').show();
+        }
+    });
+});
