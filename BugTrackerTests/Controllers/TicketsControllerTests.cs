368187e2018ba4036d5df051237fd48925bc80d8
Tom Stevens <TomStevensDev@gmail.com>
2024-05-08 20:29:57 +0100

    Feat: initial plan for tests. Arrange, Act, Assert

---
+++ BugTrackerTests/Controllers/TicketsControllerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace BugTrackerTests.Controllers;
+
+public class TicketsControllerTests
+{
+    // Arrange
+
+    // Mock repositories
+    // Projects, Tickets
+
+    // initialise a controller with mock repos
+
+    // Act
+
+    // invoke methods
+
+    // Assert
+
+    // Check what is return such as
+    // Type
+    // Assert.Equal
+}
